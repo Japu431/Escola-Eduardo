fb6fa8db795adcb1d6b22352e2c14238c27858f4
Matheus Yusuke <70763570+Japu431@users.noreply.github.com>
2022-03-17 09:46:43 -0300

    Create 5_script.php

---
+++ 5_script.php
@@ -0,0 +1,13 @@
+<h1>Esta página é para me apresentar!</h1>
+
+<?php
+    $name = "Matheus";
+    $dataNasc = '00/00/00';
+    $sobrenome = "Yusuke";
+    // Mostra na Tela >>> String + concatenação com variavel 
+    echo "Meu nome é ".$name;  // Concatenação com .<variavel>.
+    echo "<br/>";
+    echo "Nasci em $dataNasc"; // Dentro da String " $<variavel> "
+    echo "<br/>";
+    echo "Meu sobrenome é ".$sobrenome;
+?>
