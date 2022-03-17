a6e6458292be3352deb73341eb63663bac2686c5
Matheus Yusuke <70763570+Japu431@users.noreply.github.com>
2022-03-17 09:45:17 -0300

    Create 2_script.php

---
+++ 2_script.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Estamos aprendendo php!</title>
+</head>
+<body>
+    <h1>Estamos aprendendo PHP!</h1> <!-- tag de titulo -->
+    <?php
+    echo "Vamos prosseguir aprendendo PHP!! <br/>"; 
+    echo "Vai ser muito importante para o meu aprendizado!!<br/>"; // Importancia do aprendizado 
+    echo "Usarei este aprendizado para criar as páginas no meu TCC!!!"; // Terceiro comentário
+    ?>
+
+</body>
+</html>
