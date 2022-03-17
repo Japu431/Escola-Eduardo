791e4bc548e9379c353630528a4799512453df87
Matheus Yusuke <70763570+Japu431@users.noreply.github.com>
2022-03-17 09:45:44 -0300

    Create 3_script.php

---
+++ 3_script.php
@@ -0,0 +1,9 @@
+<?php
+    echo "Comentário 01<br/>"; // Comentário 1
+    echo "Comentário 02 <br/>"; # Comentário 2
+    echo "Comentário 03";
+    /*
+    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Culpa dignissimos ad ipsum possimus nostrum ducimus itaque praesentium, fugiat iusto eaque unde explicabo voluptate, dolorum quam molestias cum, ipsa laborum debitis!
+    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cumque soluta obcaecati nobis ut aperiam necessitatibus unde eius minima totam facilis deserunt distinctio beatae numquam illum sapiente, eaque cupiditate, ad nesciunt.
+    */
+?>
